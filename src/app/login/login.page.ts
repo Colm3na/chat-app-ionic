03ec6329f4c84fb6896ef1e7a8b129e9d666b75a
anagutjor <anagjordano@gmail.com>
2019-04-06 13:06:09 +0200

    fix signup and login. add error msg. add streams

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,20 +12,49 @@ export class LoginPage implements OnInit {
 
   username: string;
   password: string;
+  errorMessage: string;
 
-  constructor( private authService: AuthService ) { }
+  constructor( private authService: AuthService,
+    public toastController: ToastController,
+    private router: Router ) { }
 
   ngOnInit() {
   }
 
-  login() {
+  private login() {
     let user = {
       username: this.username,
-      email: this.password
+      password: this.password
     }
     console.log('user is', user);
-    this.authService.login(user);
-    console.log('user logged in!');
+    this.authService.login(user)
+        .subscribe( data => {
+          console.log(data);
+        }, 
+        err => {
+          console.error(err);
+          if (Array.isArray(err.error.message)) {
+            this.errorMessage = err.error.message[0].message;
+          } else {
+            this.errorMessage = err.error.message;
+          }
+          this.presentToast();
+        }, 
+        () => {
+          console.log('user logged in');
+          this.router.navigate(['/streams']);
+        })
+  }
+
+  // display error message
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: this.errorMessage,
+      color: 'danger',
+      duration: 2000,
+      position: 'top'
+    });
+    toast.present();
   }
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -11,21 +12,46 @@ export class RegisterPage implements OnInit {
   username: string;
   email: string;
   password: string;
+  errorMessage: string;
 
-  constructor( private authService: AuthService ) { }
+  constructor( private authService: AuthService,
+    public toastController: ToastController ) { }
 
   ngOnInit() {
   }
 
-  signup() {
+  private signup() {
     let user = {
       username: this.username,
       email: this.email,
       password: this.password
     }
     console.log('user is', user);
-    this.authService.signup(user);
-    console.log('user signed up!');
+    this.authService.signup(user)
+    .subscribe( data => {
+      console.log('data in authservice signup', data)
+    }, err => {
+      console.error(err);
+      if (Array.isArray(err.error.message)) {
+        this.errorMessage = err.error.message[0].message;
+      } else {
+        this.errorMessage = err.error.message;
+      }
+      this.presentToast();
+    }, () => {
+      console.log('user signed up!');
+    })
   }
 
+  // display error message
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: this.errorMessage,
+      color: 'danger',
+      duration: 2000,
+      position: 'top',
+    });
+    toast.present();
+  }
+  
 }
